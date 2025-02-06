ac151e31165d9148f52d887961fdfe311cb9c582
Brandon Harris <brandon@relentnet.com>
2025-02-06 02:22:50 -0600

    Properly grouped the Home button for MAXIMUM responsiveness.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,13 @@ import { faHouse, faUser, faAward } from '@fortawesome/free-solid-svg-icons';
 export default function NavBar() {
     return (
         <nav className='px-10 flex h-26 justify-between items-center border-b border-b-icon/30 border-t-2 border-t-gold5 bg-transparent backdrop-blur-2xl fixed top-0 left-0 w-full z-50'>
-            <div className='flex items-center justify-center border border-icon/30 rounded-l-full'>
+            <Link href="/" className='group flex items-center justify-center border border-icon/30 rounded-l-full'>
                 <div className='h-[56px] w-[56px] bg-gold4 rounded-full flex justify-center items-center'>
-                    <div className='h-[50px] w-[50px] border-2 border-black bg-radial from-blue3 from-40% to-blue4 rounded-full flex justify-center items-center'>
-                        <Link href="/">
+                    <div className='h-[50px] w-[50px] border-2 border-black bg-radial from-blue4 from-60% to-blue5 group-hover:from-blue2 rounded-full flex justify-center items-center transition duration-150'>
                             <FontAwesomeIcon icon={faHouse} className='h-7 text-gold4' />
-                        </Link>
                     </div>
                 </div>
-                <Link href="/" className="relative group">
+                <div className="relative">
                     <svg width="200" height="50" viewBox="0 0 185 50" className="-ml-6 cursor-pointer" xmlns="http://www.w3.org/2000/svg">
                         <rect x="0" y="0" width="185" height="50" className="opacity-0" />
 
@@ -21,7 +19,7 @@ export default function NavBar() {
                         <rect x="150" y="3" width="36" height="44" className="fill-hextech-black" />
 
                         <path d="M10 0 H158 L190 25 L158 50 H10 Q30 25 10 0"
-                            className="fill-blue3 transition duration-150 group-hover:fill-blue2" />
+                            className="fill-blue4 transition duration-150 group-hover:fill-blue2 group-hover:shadow-2xl shadow-blue2" />
 
                         <path d="M15 3 H156 L185 25 L156 47 H15 Q32 25 15 3"
                             className="fill-grey3 pointer-events-none" />
@@ -29,10 +27,8 @@ export default function NavBar() {
                         <text x="95" y="32" fontSize="20" fontWeight="bold" className="fill-gold1 pointer-events-none"
                             textAnchor="middle" fontFamily="serif">HOME</text>
                     </svg>
-                </Link>
-
-
-            </div>
+                </div>
+            </Link>
             <div className='flex space-x-6 h-26 justify-center items-center absolute left-1/2 transform -translate-x-1/2'>
                 <Link href="/">
                     <FontAwesomeIcon icon={faHouse} className='h-7 text-icon' />
