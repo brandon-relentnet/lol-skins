0db1321c084b16a277bb2e5f7c6a8b4a5bff90b3
Brandon Harris <brandon@relentnet.com>
2025-02-07 21:47:09 -0600

    Styled the Login page.

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -38,13 +38,13 @@ export default function LoginPage() {
     };
 
     return (
-        <div className="container mx-auto p-4 pt-36">
-            <h1 className="text-3xl font-bold mb-4">Login</h1>
+        <div className="container mx-auto p-4 pt-26 inset-0 flex flex-col items-center justify-center h-screen">
+            <h1 className="text-5xl font-bold font-serif mb-2 text-gold2">Login</h1>
             {message && <p className="mb-4 text-green-600">{message}</p>}
             {errorMsg && <p className="mb-4 text-red-600">{errorMsg}</p>}
-            <form onSubmit={handleSubmit} className="max-w-md">
+            <form onSubmit={handleSubmit} className="max-w-lg w-full">
                 <div className="mb-4">
-                    <label htmlFor="email" className="block font-medium">
+                    <label htmlFor="email" className="block text-lg text-grey1 mb-1">
                         Email:
                     </label>
                     <input
@@ -53,12 +53,12 @@ export default function LoginPage() {
                         placeholder="user@example.com"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        className="border px-2 py-1 w-full"
+                        className="bg-hextech-black/30 w-full border-2 border-transparent outline-icon/30 outline -outline-offset-2 hover:border-icon hover:border-2 transition duration-150 font-serif text-grey1 hover:text-gold1 text-lg font-bold px-8 py-2 shadow-lg"
                         required
                     />
                 </div>
-                <div className="mb-4">
-                    <label htmlFor="password" className="block font-medium">
+                <div className="mb-6">
+                    <label htmlFor="password" className="block text-lg text-grey1 mb-1">
                         Password:
                     </label>
                     <input
@@ -67,19 +67,21 @@ export default function LoginPage() {
                         placeholder="Your password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        className="border px-2 py-1 w-full"
+                        className="bg-hextech-black/30 w-full border-2 border-transparent outline-icon/30 outline -outline-offset-2 hover:border-icon hover:border-2 transition duration-150 font-serif text-grey1 hover:text-gold1 text-lg font-bold px-8 py-2 shadow-lg"
                         required
                     />
                 </div>
-                <button
-                    type="submit"
-                    className="bg-blue-500 text-white px-4 py-2 rounded"
-                >
-                    Sign In
-                </button>
+                <div className="mb-14 text-center">
+                    <button
+                        type="submit"
+                        className="bg-hextech-black/30 w-1/2 border-2 border-transparent outline-icon/30 outline -outline-offset-2 hover:border-icon hover:border-2 transition duration-150 font-serif text-grey1 hover:text-gold1 text-lg font-bold px-8 py-4 shadow-lg"
+                    >
+                        Sign In
+                    </button>
+                </div>
             </form>
-            <Link href="/auth/register" className="mt-4 text-blue-500">Register</Link>
-            <Link href="/auth/forgot" className="mt-4 text-blue-500">Forgot Password</Link>
+            <Link href="/auth/register" className="bg-hextech-black/30 border-2 border-transparent outline-icon/30 outline -outline-offset-2 hover:border-icon hover:border-2 transition duration-150 font-serif text-grey1 hover:text-gold1 text-lg font-bold px-8 py-4 shadow-lg">Register</Link>
+            <Link href="/auth/forgot" className="mt-4 text-grey2 hover:text-grey1 transition duration-150">Forgot Password</Link>
         </div>
     );
 }
